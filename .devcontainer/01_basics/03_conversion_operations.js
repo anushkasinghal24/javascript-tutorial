c0879b5fea8100dabd11b229490e74bb635f83ec
anushkasinghal24 <anushka.singhal_cs22@gla.ac.in>
2024-08-31 11:21:17 +0530

    Conversion of dataTypes

---
+++ .devcontainer/01_basics/03_conversion_operations.js
@@ -0,0 +1,19 @@
+let score ="Anushka";
+console.log(typeof(score));
+console.log(typeof score);
+
+let newVar = Number(score);
+console.log(typeof newVar);
+console.log(newVar);
+
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1  , false => 0
+
+let Isloggedin = 0;
+let booleanIsloggedin = Boolean(Isloggedin);
+console.log(typeof(booleanIsloggedin));
+console.log(booleanIsloggedin);
+
+// for a empty string ("") the output will be false , and for a string ("Anushka") output will be true.
