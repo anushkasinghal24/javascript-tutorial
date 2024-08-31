2435ecb0b0bed5392830440cdaf171ceca52f16e
anushkasinghal24 <anushka.singhal_cs22@gla.ac.in>
2024-08-31 13:56:02 +0530

    Conversion of dataTypes

--- .devcontainer/01_basics/03_conversion_operations.js
+++ .devcontainer/01_basics/03_conversion_operations.js
@@ -16,4 +16,16 @@ let booleanIsloggedin = Boolean(Isloggedin);
 console.log(typeof(booleanIsloggedin));
 console.log(booleanIsloggedin);
 
-// for a empty string ("") the output will be false , and for a string ("Anushka") output will be true.
+// for a empty string ("") the output will be false .
+//for a string ("Anushka") output will be true.
+
+/*************************************** Operations*********************************/
+ let str1 = "Anushka";
+ let str2 = "Singhal";
+ let str3 = str1 + " " + str2 
+ console.log (str3);
+
+ console.log("1" + 2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);  // priority will be a string
+ console.log(1 + 2 + "2");  // priority will be a number
