23364b2b09723cf75c80a28ac1b15fc9c021f3e6
anushkasinghal24 <anushka.singhal_cs22@gla.ac.in>
2024-08-31 14:43:30 +0530

    Comparison of Dataypes

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+
+// There should be a compariosn between the same data types .
+/*     ************** We should avoid these types of conversion ******
+// The output of the below code sometimes can be true , false and NaN 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//The output of the below code will always be false
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);*/
+
